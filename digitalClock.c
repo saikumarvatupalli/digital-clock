ef2bc4c544e80265f0c6a9fe8382467adbbf12c9
saikumarvatupalli <saikumarvatupalli20025@gmail.com>
2023-04-02 10:54:22 +0530

    my commit

---
+++ digitalClock.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <windows.h>
+int main()
+{
+  int h, m, s;
+  int d = 1000;
+  printf("Set the time : \n");
+  scanf("%d%d%d", &h, &m, &s);
+  if (h > 24 || m > 60 || s > 60)
+  {
+    printf("No time ! \n");
+    exit(0);
+  }
+  while (1)
+  {
+    s++;
+    if (s > 59)
+    {
+      m++;
+      s = 0;
+    }
+    if (m > 59)
+    {
+      h++;
+      m = 0;
+    }
+    if (h > 24)
+    {
+      h = 1;
+    }
+    printf("\n Clock :");
+    printf("\n %02d:%02d:%02d", h, m, s);
+    Sleep(d);
+    system("cls");
+  }
+}
